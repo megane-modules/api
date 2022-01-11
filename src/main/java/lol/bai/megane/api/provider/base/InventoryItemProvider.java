91e8622ac3e0371675339d4acd401ef67b5d9ec0
deirn <deirn@bai.lol>
2022-01-11 13:34:04 +0700

    move getInventory call to setContext

--- src/main/java/lol/bai/megane/api/provider/base/InventoryItemProvider.java
+++ src/main/java/lol/bai/megane/api/provider/base/InventoryItemProvider.java
@@ -1,8 +1,11 @@
 package lol.bai.megane.api.provider.base;
 
 import lol.bai.megane.api.provider.ItemProvider;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,8 +17,14 @@ public abstract class InventoryItemProvider<T> extends ItemProvider<T> {
     protected abstract Inventory getInventory();
 
     @Override
-    public boolean hasItems() {
+    public void setContext(World world, BlockPos pos, PlayerEntity player, T t) {
+        super.setContext(world, pos, player, t);
+
         this.inventory = getInventory();
+    }
+
+    @Override
+    public boolean hasItems() {
         return inventory != null;
     }
 
