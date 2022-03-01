b05c508825a3c9a7d8d980cdbc117b0296100b89
deirn <deirn@bai.lol>
2022-03-01 20:10:49 +0700

    expose HitResult on providers

--- src/main/java/lol/bai/megane/api/provider/AbstractProvider.java
+++ src/main/java/lol/bai/megane/api/provider/AbstractProvider.java
@@ -1,21 +1,24 @@
 package lol.bai.megane.api.provider;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.ApiStatus;
 
 public abstract class AbstractProvider<T> {
 
     private World world;
     private BlockPos pos;
+    private HitResult hitResult;
     private PlayerEntity player;
     private T object;
 
-    public void setContext(World world, BlockPos pos, PlayerEntity player, T t) {
-        this.world = world;
-        this.pos = pos;
-        this.player = player;
-        this.object = t;
+    /**
+     * The earliest method to get called by the runtime.
+     * Compute additional context here.
+     */
+    protected void init() {
     }
 
     public boolean blockOtherProvider() {
@@ -30,6 +33,10 @@ public abstract class AbstractProvider<T> {
         return pos;
     }
 
+    protected final HitResult getHitResult() {
+        return hitResult;
+    }
+
     protected final PlayerEntity getPlayer() {
         return player;
     }
@@ -41,6 +48,29 @@ public abstract class AbstractProvider<T> {
     @SuppressWarnings("unchecked")
     protected final <C> C getObjectCasted() {
         return (C) object;
+    }
+
+    /**
+     * @see #init()
+     */
+    @ApiStatus.Internal
+    public final void setContext(World world, BlockPos pos, HitResult hitResult, PlayerEntity player, T t) {
+        this.world = world;
+        this.pos = pos;
+        this.hitResult = hitResult;
+        this.player = player;
+        this.object = t;
+
+        setContext(world, pos, player, t);
+        init();
+    }
+
+    /**
+     * @deprecated override {@link #init()} instead.
+     */
+    @Deprecated
+    @ApiStatus.ScheduledForRemoval(inVersion = "1.19")
+    public void setContext(World world, BlockPos pos, PlayerEntity player, T t) {
     }
 
 }
--- src/main/java/lol/bai/megane/api/provider/base/InventoryItemProvider.java
+++ src/main/java/lol/bai/megane/api/provider/base/InventoryItemProvider.java
@@ -1,11 +1,8 @@
 package lol.bai.megane.api.provider.base;
 
 import lol.bai.megane.api.provider.ItemProvider;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,9 +14,7 @@ public abstract class InventoryItemProvider<T> extends ItemProvider<T> {
     protected abstract Inventory getInventory();
 
     @Override
-    public void setContext(World world, BlockPos pos, PlayerEntity player, T t) {
-        super.setContext(world, pos, player, t);
-
+    protected void init() {
         this.inventory = getInventory();
     }
 
